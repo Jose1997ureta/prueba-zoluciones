47946c1f0ae2f13858355f41c76e2111b46d1adc
José <dev02.azzdigital@gmail.com>
2021-10-28 11:35:10 -0500

    Se agrego el boton de monto a fixed

--- src/components/screens/monto/montoComponent.js
+++ src/components/screens/monto/montoComponent.js
@@ -4,25 +4,31 @@ import "./monto.scss";
 export const MontoComponent = () => {
 	return (
 		<div className="monto__container">
-			<p className="monto__title">Monto</p>
-			<p className="monto__price">$35.00</p>
-			<p className="monto__month">mensuales</p>
-			<div className="monto__line" />
-			<p className="oferta__title">El precio incluye:</p>
-			<div className="oferta__item">
-				<FaCheck className="oferta__item--icon" />
-				<p className="oferta__item--text">Llanta de respuesto</p>
-			</div>
-			<div className="oferta__item">
-				<FaCheck className="oferta__item--icon" />
-				<p className="oferta__item--text">Analisis de motor</p>
-			</div>
-			<div className="oferta__item">
-				<FaCheck className="oferta__item--icon" />
-				<p className="oferta__item--text">Aros gratis</p>
-			</div>
-			<div style={{ marginTop: 32 }}>
-				<ButtonComponent variant="primary" text="Lo quiero" />
+			<div className="monto">
+				<p className="monto__title">Monto</p>
+				<div className="monto__price">
+					<p className="monto__price--text">$35.00</p>
+					<p className="monto__price--month">mensuales</p>
+				</div>
+				<div className="monto__line" />
+				<div className="oferta">
+					<p className="oferta__title">El precio incluye:</p>
+					<div className="oferta__item">
+						<FaCheck className="oferta__item--icon" />
+						<p className="oferta__item--text">Llanta de respuesto</p>
+					</div>
+					<div className="oferta__item">
+						<FaCheck className="oferta__item--icon" />
+						<p className="oferta__item--text">Analisis de motor</p>
+					</div>
+					<div className="oferta__item">
+						<FaCheck className="oferta__item--icon" />
+						<p className="oferta__item--text">Aros gratis</p>
+					</div>
+				</div>
+				<div className="monto__button">
+					<ButtonComponent variant="primary" text="Lo quiero" />
+				</div>
 			</div>
 		</div>
 	);
